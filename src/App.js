fbe180b227447208538dc832aefd04de32a3911f
Ben McIntyre <ben@bens-mbp.attlocal.net>
2023-01-03 12:17:00 -0800

    Finished Part 1...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
 
