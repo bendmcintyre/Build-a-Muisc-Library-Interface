29863e0a6276f1a493d57059c6dcc9973f315d09
Ben McIntyre <ben@bens-mbp.attlocal.net>
2023-01-03 12:16:04 -0800

    Finished Part 1

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GalleryItem from "./GalleryItem";
+
+function Gallery(props) {
+  return (
+    <div>
+      <GalleryItem />
+    </div>
+  );
+}
+
+export default Gallery;
