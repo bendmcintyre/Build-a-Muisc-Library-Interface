ce878a4f72fc7c3f69e1601a4530ab744fd5a382
Ben McIntyre <ben@bens-mbp.attlocal.net>
2023-01-03 12:47:38 -0800

    Part 2 Finished

--- src/App.js
+++ src/App.js
@@ -2,34 +2,41 @@ import React, { useEffect, useState } from "react";
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
 
-function App() {
+const App = () => {
   let [search, setSearch] = useState("");
   let [message, setMessage] = useState("Search for Music!");
   let [data, setData] = useState([]);
 
+  const API_URL = "https://itunes.apple.com/search?term=";
+
   useEffect(() => {
-    const fetchData = async () => {
-      document.title = `${search} Music`;
-      const response = await fetch(
-        "https://itunes.apple.com/search?term=the%20grateful%20dead"
-      );
-      const resData = await response.json();
-      if (resData.results.length > 0) {
-        setData(resData.results);
-      } else {
-        setMessage("Not Found");
-      }
-    };
-    fetchData();
+    if (search) {
+      const fetchData = async () => {
+        document.title = `${search} music`;
+        const response = await fetch(API_URL + search);
+        const resData = await response.json();
+        if (resData.results.length > 0) {
+          return setData(resData.results);
+        } else {
+          return setMessage("Not Found.");
+        }
+      };
+      fetchData();
+    }
   }, [search]);
+
+  const handleSearch = (e, term) => {
+    e.preventDefault();
+    setSearch(term);
+  };
 
   return (
     <div>
-      <SearchBar />
+      <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery />
+      <Gallery data={data} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
-function SearchBar(props) {
-  let [searchTerm, setSearchTerm] = useState("");
 
-  return (
-    <form>
-      <input type="text" placeholder="Enter a search term here" />
-      <input type="submit" />
-    </form>
-  );
+function SearchBar(props){
+    let [searchTerm, setSearchTerm] = useState('')
+
+    return (
+        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+
+            <input type="text" placeholder="Enter a search term here"
+            onChange={(e) => setSearchTerm(e.target.value)} />
+
+            <input type="submit" />
+
+        </form>
+    )
 }
 
 export default SearchBar;
